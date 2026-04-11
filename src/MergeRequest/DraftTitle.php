e79569a752e8ecf4cd24ebd8822711e9aea6023c
Kanstantsin Mesnik <haspadar@gmail.com>
2026-04-12 02:20:51 +0300

    Toggle Draft: prefix on merge request titles

---
+++ src/MergeRequest/DraftTitle.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Goblin\MergeRequest;
+
+/**
+ * Manages Draft: prefix on merge request titles.
+ *
+ * @psalm-api
+ */
+final readonly class DraftTitle
+{
+    /**
+     * Stores the original title.
+     */
+    public function __construct(private string $title) {}
+
+    /**
+     * Returns title with Draft: prefix added.
+     */
+    public function drafted(): string
+    {
+        if (preg_match('/^(Draft:|WIP:)\s/i', $this->title) === 1) {
+            return $this->title;
+        }
+
+        return 'Draft: ' . $this->title;
+    }
+
+    /**
+     * Returns title with Draft:/WIP: prefix removed.
+     */
+    public function ready(): string
+    {
+        $cleaned = preg_replace('/^(Draft:|WIP:)\s*/i', '', $this->title);
+
+        return $cleaned ?? $this->title;
+    }
+}
