5c5ac28c103bbe5e5e39023145a0005f6a5cd066
Kanstantsin Mesnik <haspadar@gmail.com>
2026-04-15 10:30:18 +0300

    #66: Document branch-rules config format

--- .goblin.example.php
+++ .goblin.example.php
@@ -6,6 +6,11 @@ return [
     'jira-token' => '...',
     'project-regex' => '/^([A-Z]+)-\d+/',
     'protected-branches' => ['main', 'dev', 'stage', 'beta', 'master'],
+    'branch-rules' => [
+        'beta'  => ['match' => '/(?P<major>\d+)\.(?P<minor>\d+)\.1$/'],
+        'stage' => ['match' => '/{major}\.{minor+1}\.0$/'],
+        'default' => 'dev',
+    ],
     'gitlab-url' => 'https://gitlab.example.com',
     'gitlab-token' => '...',
     'container' => 'your-app',
